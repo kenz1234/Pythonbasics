a02ff6daa29788f21f5c935f63ba0bb5fd747f92
kenz1234 <144541271+kenz1234@users.noreply.github.com>
2025-08-19 11:40:12 +0530

    Add files via upload

---
+++ assignmentbasics.py
@@ -0,0 +1,80 @@
+
+
+# i=1
+# for i in range (11):
+#     print(i)
+
+
+# i=1
+# while i <= 20:
+#      if i % 2 == 0:
+#          print(i)
+#      i += 1
+    
+
+
+# i=1
+# while i<=10:
+#     a = i * 2
+#     print(a)
+#     i+=1
+
+
+
+# i=1
+# for i in range (11):
+#     a = i * 2
+#     print(a)
+
+
+
+# n = int(input("Enter a number to find factorial : "))
+# fact = 1
+# for i in range (1, n+1):
+#     fact *= i
+#     print(fact)
+
+
+# i=0
+# while i <= 20:
+#      i += 1
+#      if i == 6:
+#          break
+#      print(i)
+
+
+
+# i=0
+# while i <= 9:
+#      i += 1
+#      if i == 7:
+#          continue
+#      print(i)
+
+
+
+# i=1
+# while i <= 20:
+#      i += 1
+#      if i % 3 == 0:
+#          continue
+#      print(i)
+
+
+
+# i=0
+# while i <= 100:
+#      i += 1
+#      if i % 12 == 0:
+#           break
+#      print(i)
+
+
+
+
+i=0
+for i in range (100):
+    if 9 <= i <= 19:
+        continue
+    i += 1
+    print(i)
